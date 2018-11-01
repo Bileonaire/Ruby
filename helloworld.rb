8f90c9f6f39538cbdaebf1a873b462b1533bc929
Bileonaire <leonkioko@Leons-MacBook-Pro.local>
2018-11-01 13:04:48 +0300

    Hello world Ruby

--- helloworld.rb
+++ helloworld.rb
@@ -1,4 +1,4 @@
 puts "Hello world"
 puts("This", "prints", 1, "by", 1)
-sleep(5)
-print("This ", "prints ", "on ", "the ", "same ", "line ")
+sleep 5
+print "This ", "prints ", "on ", "the ", "same ", "line "
